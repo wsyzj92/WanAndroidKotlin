5bbefa7350bd5742b38977c36e4e5d174fd932d3
wsyzj_92 <wsyzj_92@163.com>
2019-12-13 15:25:08 +0800

    第一次提交代码

---
+++ app/src/main/java/com/wsyzj/wanandroidkotlin/common/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.wsyzj.wanandroidkotlin.common
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * <pre>
+ *     author : 焦洋
+ *     e-mail : wsyzj_92@163.com
+ *     time   : 2019/12/13
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+class BaseActivity : AppCompatActivity() {
+
+}
